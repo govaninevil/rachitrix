7d14b4fc273193be7168f52559bd9434476f6bc6
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2023-07-18 18:54:38 +0530

    main

--- script/script.js
+++ script/script.js
@@ -110,4 +110,50 @@ $(document).on('ready', function () {
           // instead of a settings object
         ]
       });
+
+      $('#work-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+      });
+
+      $('#recent-news-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      $('#parentVerticalTab').easyResponsiveTabs({
+        type: 'vertical', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        closed: 'accordion', // Start closed if in accordion view
+        tabidentify: 'hor_1', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo2');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
 });
