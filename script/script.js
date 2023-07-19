d1118a99e6943c7c3f533f073c0b86929d6d613a
Codexs <codexsdeveloper@gmail.com>
2023-07-20 02:53:41 +0530

    main

--- script/script.js
+++ script/script.js
@@ -1,159 +1,166 @@
-$(document).on('ready', function () {
+$(document).on("ready", function () {
+  $("#mobile").on("click", function () {
+    $("body").toggleClass("mobile");
+  });
 
+  $("#ltr-slider").slick({
+    dots: false,
+    navs: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 8000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1000,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-    $("#mobile").on("click", function () {
-        $("body").toggleClass("mobile");
-    });
+  $("#rtl-slider").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 8000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1000,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-    $('#ltr-slider').slick({
-        dots: false,
-        navs: false,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 8000,
-        cssEase: 'linear',
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,
-              autoplaySpeed: 1000
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+  $("#service-slider").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1.6,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-      $('#rtl-slider').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 8000,
-        cssEase: 'linear',
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,
-              autoplaySpeed: 1000
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+  $("#work-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    fade: true,
+    cssEase: "linear",
+    slidesToShow: 1,
+  });
 
+  $("#recent-news-slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
 
-      $('#service-slider').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 1.6,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-      $('#work-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        fade: true,
-        cssEase: 'linear',
-        slidesToShow: 1,
-      });
-
-      $('#recent-news-slider').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 2.5,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
-
-      $('#parentVerticalTab').easyResponsiveTabs({
-        type: 'vertical', //Types: default, vertical, accordion
-        width: 'auto', //auto or any width like 600px
-        fit: true, // 100% fit in a container
-        closed: 'accordion', // Start closed if in accordion view
-        tabidentify: 'hor_1', // The tab groups identifier
-        activate: function(event) { // Callback function if tab is switched
-            var $tab = $(this);
-            var $info = $('#nested-tabInfo2');
-            var $name = $('span', $info);
-            $name.text($tab.text());
-            $info.show();
-        }
-    });
+  $("#parentVerticalTab").easyResponsiveTabs({
+    type: "vertical", //Types: default, vertical, accordion
+    width: "auto", //auto or any width like 600px
+    fit: true, // 100% fit in a container
+    closed: "accordion", // Start closed if in accordion view
+    tabidentify: "hor_1", // The tab groups identifier
+    activate: function (event) {
+      // Callback function if tab is switched
+      var $tab = $(this);
+      var $info = $("#nested-tabInfo2");
+      var $name = $("span", $info);
+      $name.text($tab.text());
+      $info.show();
+    },
+  });
 });
